8ef7e5de067224e527d1f0ac3986032e43e4a46e
אודיה סעדון <osadoun@g.jct.ac.il>
2021-03-14 00:43:23 +0200

    add throws exception in polygon class and triangle class

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -6,8 +6,9 @@ public class Triangle extends Polygon implements Geometry
 {
 	/**
 	 * Constructor that receives 3 points and calls to the constructor of the base class
+	 * @throws Exception 
 	 * */
-	public Triangle(Point3D p1,Point3D p2,Point3D p3) 
+	public Triangle(Point3D p1,Point3D p2,Point3D p3) throws Exception 
 	{
 		super(p1,p2,p3);
 		
