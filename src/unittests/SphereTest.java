512328881368e387b577527068acf709b81ce840
אודיה סעדון <osadoun@g.jct.ac.il>
2021-03-14 00:27:10 +0200

    add fail to the catch if the vectors not 0 and throws exception in the class spheraTest

--- src/unittests/SphereTest.java
+++ src/unittests/SphereTest.java
@@ -25,15 +25,16 @@ public class SphereTest {
 	{
 		try
 		{
-		double radius=5;// הנקודה p שעל הספרה תהיה 5,2,6
-		Point3D center=new Point3D(1, 2, 3);		
-		Sphere mySphere=new Sphere(center, radius);
-		Vector normal=new Vector(4/5, 0, 3/5);
-		assertFalse("ERROR: getNormal() function creates an Incorrect normal", mySphere.getNormal(center) !=normal);
+			// ============ Equivalence Partitions Tests ==============
+			double radius=5;// הנקודה p שעל הספרה תהיה 5,2,6
+			Point3D center=new Point3D(1, 2, 3);		
+			Sphere mySphere=new Sphere(center, radius);
+			Vector normal=new Vector(4/5, 0, 3/5);
+			assertFalse("ERROR: getNormal() function creates an Incorrect normal", mySphere.getNormal(center) !=normal);
 		}
 		catch(Exception ex)
 		{
-			
+			fail("for vectors that not zero vector does not need throw an exception");
 		}
 		
 		
