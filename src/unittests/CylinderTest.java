03d782f06b0e086f8e50c751372d06391161eb2a
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-14 22:01:55 +0200

    Begin of tests for cylinder

--- src/unittests/CylinderTest.java
+++ src/unittests/CylinderTest.java
@@ -7,6 +7,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import geometries.Cylinder;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
 /**
  * @author Home
  *
@@ -17,8 +22,23 @@ public class CylinderTest {
 	 * Test method for {@link geometries.Cylinder#getNormal(primitives.Point3D)}.
 	 */
 	@Test
-	public void testGetNormal() {
-		fail("Not yet implemented");
+	public void testGetNormal() 
+	{		
+		try
+		{
+		double radius =3;
+		Point3D p=new Point3D(1, 7, 4);
+		Vector v=new Vector(p);
+		Ray myRay=new Ray(p, v);
+		double height=4;
+		Cylinder myCylinder=new Cylinder(radius,myRay,height);
+		assertFalse("ERROR: getNormal() function creates an Incorrect vector", myCylinder.getNormal(p) !=v);
+		}
+		catch(Exception ex)
+		{
+			
+		}
+		
 	}
 
 }
