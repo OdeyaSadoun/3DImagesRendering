609d9437c05a6e9658dcbcaa5723c44f4d4ada89
אודיה סעדון <osadoun@g.jct.ac.il>
2021-03-14 23:58:30 +0200

    triangle test

--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -28,13 +28,21 @@ public class TriangleTest
         // TC01: There is a simple single test here
 		try
 		{
-			Triangle myTriangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
+			//Triangle myTriangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
 			
-			Vector v = new Vector(new Point3D(0, 0, 1).subtract(new Point3D(1, 0, 0)).getHead());
+			//Vector v = new Vector(new Point3D(0, 0, 1).subtract(new Point3D(1, 0, 0)).getHead());
 			
-			Vector normal=new Vector(1, 1, 1);
-			assertEquals("bad normal to triangle", isZero(myTriangle.getNormal(new Point3D(0, 0, 1)).dotProduct(v)));
+			//Vector normal=new Vector(1, 1, 1);
+			//assertEquals("bad normal to triangle", isZero(myTriangle.getNormal(new Point3D(0, 0, 1)).dotProduct(v)));
 
+			
+				   Triangle pl;
+					pl = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
+					double sqrt3 = Math.sqrt(1d / 3);
+				    assertEquals("Bad normal to trinagle", new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point3D(0, 0, 1)));
+					
+			
+			
 			//assertFalse("ERROR: getNormal() function creates an Incorrect normal", myTriangle.getNormal(new Point3D(0, 0, 1)) !=v);
 		}
 		catch(Exception ex) 
