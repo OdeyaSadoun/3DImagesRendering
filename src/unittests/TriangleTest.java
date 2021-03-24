f06a87ba5d024ab4f2bd4372d090b338e2d57fb1
אודיה סעדון <osadoun@g.jct.ac.il>
2021-03-24 11:08:59 +0200

    change in the line assertequal in triangletest calss

--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -26,22 +26,11 @@ public class TriangleTest
         // TC01: There is a simple single test here
 		try
 		{
-			//Triangle myTriangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
-			
-			//Vector v = new Vector(new Point3D(0, 0, 1).subtract(new Point3D(1, 0, 0)).getHead());
-			
-			//Vector normal=new Vector(1, 1, 1);
-			//assertEquals("bad normal to triangle", isZero(myTriangle.getNormal(new Point3D(0, 0, 1)).dotProduct(v)));
-
-			
-				   Triangle pl;
-					pl = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
-					double sqrt3 = Math.sqrt(1d / 3);
-				    assertEquals("Bad normal to trinagle", new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point3D(0, 0, 1)));
-					
-			
-			
-			//assertFalse("ERROR: getNormal() function creates an Incorrect normal", myTriangle.getNormal(new Point3D(0, 0, 1)) !=v);
+			Triangle myTriangle = new Triangle(new Point3D(0, 0, 1), new Point3D(1, 0, 0), new Point3D(0, 1, 0));
+			double sqrt3 = Math.sqrt(1d / 3);
+			Vector normal=new Vector(sqrt3, sqrt3, sqrt3);
+		    assertEquals("Bad normal to trinagle", normal , myTriangle.getNormal(new Point3D(0, 0, 1)));
+	
 		}
 		catch(Exception ex) 
 		{
