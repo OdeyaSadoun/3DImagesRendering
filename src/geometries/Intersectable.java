a858d482bb24922ffdc71d12c4f87710966c458e
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-28 22:15:33 +0300

    add interface intersectable for geometries package

---
+++ src/geometries/Intersectable.java
@@ -0,0 +1,17 @@
+package geometries;
+
+import primitives.Ray;
+
+import java.util.List;
+
+import primitives.Point3D;
+/**
+ * Interface for Intsersections
+ * @author Tamar Gavrieli 322533977 and Odeya Sadoun 212380406
+ *
+ */
+public interface Intersectable 
+{
+	List<Point3D> findIntsersections(Ray ray);
+
+}
