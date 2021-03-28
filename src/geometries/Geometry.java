87da00ee82149e1b80621c371be8b6e374df3261
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-28 22:27:36 +0300

    Changing the geometry interface inherited from the Intersectable interface

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -4,7 +4,7 @@ package geometries;
 import primitives.Vector;
 import primitives.Point3D;
 
-public interface Geometry 
+public interface Geometry extends Intersectable
 {
 	Vector getNormal(Point3D point) throws Exception;
 
