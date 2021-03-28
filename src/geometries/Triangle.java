f4cd0751c3c465f46b168aa2d02aa62c13c882be
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-28 22:13:18 +0300

    add javacode for Triangle class

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,11 +1,17 @@
 package geometries;
-
 import primitives.Point3D;
-
+/**
+ * @author Tamar Gavrieli 322533977 and Odeya Sadoun 212380406
+ * Class for Triangle
+ */
 public class Triangle extends Polygon implements Geometry
 {
 	/**
 	 * Constructor that receives 3 points and calls to the constructor of the base class
+	 * 
+	 * @param p1 Point3D
+	 * @param p2 Point3D
+	 * @param p3 Point3D
 	 * @throws Exception 
 	 * */
 	public Triangle(Point3D p1,Point3D p2,Point3D p3) throws Exception 
