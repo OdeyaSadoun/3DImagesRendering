c32c94024f806e44af9bd32cddb770c603552e7b
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-28 23:57:10 +0300

    change from Targil 2 according to Dan Zilbershtein- add BVA

--- src/unittests/TubeTest.java
+++ src/unittests/TubeTest.java
@@ -30,8 +30,16 @@ public class TubeTest
 			Tube myTube=new Tube(5, myRay);
 			Vector normal = new Vector(1,0,0);
 			assertEquals("Bad normal to tube", normal , myTube.getNormal(new Point3D(3,1,0)));
-		
+			
+			// =============== Boundary Values Tests ==================
+			// Check if the point is on the circumference of the circle, 
+			//check if the distance between the point and the center point is the same as the radius. 
+			//If so the point must be on the circumference of the circle.
+			double lengthNormal=Math.sqrt(21);
+			normal=new Vector(4d/lengthNormal, 0, Math.sqrt(5)/lengthNormal);
+			assertEquals("Bad normal to tube", normal , myTube.getNormal(new Point3D(5,4,Math.sqrt(5))));
 		} 
+		
 		catch (Exception e) 
 		{		
 			fail("for vectors that not zero vector does not need throw an exception");
