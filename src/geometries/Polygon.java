aa06fef4d991f86f5d98606eccdbbb39993f2146
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-29 20:34:46 +0300

    some changes in polygon class

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -90,7 +90,7 @@ public class Polygon implements Geometry
 	}
 
 	@Override
-	public List<Point3D> findIntsersections(Ray ray) {
+	public List<Point3D> findIntersections(Ray ray) {
 		// TODO Auto-generated method stub
 		return null;
 	}
