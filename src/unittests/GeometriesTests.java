d0fb17ab865cdbcc9814829ecb9777b5728a2fb0
אודיה סעדון <osadoun@g.jct.ac.il>
2021-03-29 20:42:25 +0300

    begin of geometries test class

---
+++ src/unittests/GeometriesTests.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for geometries.Geometries class
+ * 
+ * @author אודיה סעדון
+ *
+ */
+public class GeometriesTests 
+{
+
+	/**
+	 * Test method for {@link geometries.Geometries#add(geometries.Intersectable[])}.
+	 */
+	@Test
+	public void testAdd() 
+	{
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link geometries.Geometries#findIntsersections(primitives.Ray)}.
+	 */
+	@Test
+	public void testFindIntsersections() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
