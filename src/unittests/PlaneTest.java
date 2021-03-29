af10f675662108c26c037ea81f51b6a01c3076c3
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-29 20:39:55 +0300

    add BVA in planeTest

--- src/unittests/PlaneTest.java
+++ src/unittests/PlaneTest.java
@@ -15,6 +15,32 @@ import primitives.Vector;
  */
 public class PlaneTest
 {
+	@Test
+	public void testPlaneConstructor()
+	{
+		try
+		{
+			Point3D p1=new Point3D(1,2,3);
+			Point3D p2=new Point3D(1,2,3);
+			Point3D p3=new Point3D(1,7,5);
+			Plane myPlane =new Plane(p1, p2 , p3);
+			//assertThrows("failure text", IllegalArgumentException.class, () -> function call);
+			fail ("The points coalesce and the constructor should send an exception");
+		}
+		catch(Exception ex) {}
+		
+		try
+		{
+			Point3D p1=new Point3D(1,2,3);
+			Point3D p2=new Point3D(2,4,6);
+			Point3D p3=new Point3D(1,7,5);
+			Plane myPlane =new Plane(p1, p2 , p3);
+			//assertThrows("failure text", IllegalArgumentException.class, () -> function call);
+			fail ("The dots are on the same line");
+		}
+		catch(Exception ex) {}
+		
+	}
 
 	/**
 	 * Test method for {@link geometries.Plane#getNormal(primitives.Point3D)}.
@@ -34,7 +60,7 @@ public class PlaneTest
 			double normalLength=Math.sqrt(38)*3;
 			Vector normal=new Vector(-9/normalLength, -6/normalLength, 15/normalLength);
 			assertEquals("Bad normal to plane", normal, myPlane.getNormal());
-
+			// =============== Boundary Values Tests ==================
 		
 		}
 		catch(Exception ex)
@@ -42,5 +68,15 @@ public class PlaneTest
 			fail("for vectors that not zero vector does not need throw an exception");
 		}
 	}
+	
+	/**
+	 * Test method for {@link geometries.Cylinder#getNormal(primitives.Point3D)}.
+	 */
+	@Test
+	public void testfindIntersections() 
+	{		
+	}
+	
+	
 
 }
