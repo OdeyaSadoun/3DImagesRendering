6ae63568400e632bccc214492b2cdd89cba30805
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-03-29 20:36:44 +0300

    changes in function findintersection

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -64,7 +64,8 @@ public class Sphere implements Geometry
 	}
 
 	@Override
-	public List<Point3D> findIntsersections(Ray ray) {
+	public List<Point3D> findIntersections(Ray ray) 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
