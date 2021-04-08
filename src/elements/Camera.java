206d0073202f1c1c3741d859aab08e9ed8cb561e
אודיה סעדון <osadoun@g.jct.ac.il>
2021-04-08 20:01:26 +0300

    add elements pacage and camera class

---
+++ src/elements/Camera.java
@@ -0,0 +1,20 @@
+
+package elements;
+
+/**
+ * @author אודיה סעדון
+ *
+ */
+public class Camera 
+{
+	
+
+	/**
+	 * 
+	 */
+	public Camera() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
