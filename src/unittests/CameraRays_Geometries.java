75ad932fd629687738836bbc3351a2e3193a3be1
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-04-11 14:56:14 +0300

    continue with CameraRays

--- src/unittests/CameraRays_Geometries.java
+++ src/unittests/CameraRays_Geometries.java
@@ -13,7 +13,9 @@ import org.junit.Test;
 import elements.Camera;
 import geometries.Geometries;
 import geometries.Intersectable;
+import geometries.Plane;
 import geometries.Sphere;
+import geometries.Triangle;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
@@ -73,26 +75,30 @@ public class CameraRays_Geometries
 		
 		Sphere sphere=new Sphere(new Point3D(0,0,-3), 1);
 		Camera camera = new Camera(new Point3D(0,0,0), new Vector(0,0,-1), new Vector(0,1,0)).setDistance(1).setViewPlaneSize(3, 3);
-		assertEquals("the intersections are dont correct", findIntersectionPoints(camera, sphere).size() , camera.constructRayThroughPixel(3, 3, 0, 0));
+		assertEquals("The count of intersections are not correct", 2, findIntersectionPoints(camera, sphere).size());
 
 		
 		//TC02:18 intersection points
 		
 		sphere=new Sphere(new Point3D(0,0,-2.5), 2.5);
 		camera = new Camera(new Point3D(0,0,0.5), new Vector(0,0,-1), new Vector(0,1,0)).setDistance(1).setViewPlaneSize(3, 3);	
+		assertEquals("The count of intersections are not correct", 18, findIntersectionPoints(camera, sphere).size());
 		
 		//TC03:10 intersection points
 		
 		sphere=new Sphere(new Point3D(0,0,-2), 2);
+		assertEquals("The count of intersections are not correct", 10, findIntersectionPoints(camera, sphere).size());
 		
 		//TC04:0 intersection points
 		
 		sphere=new Sphere(new Point3D(0,0,1), 0.5);
+		assertNull("The count of intersections are not correct", findIntersectionPoints(camera, sphere).size());
 		
 		//TC05:9 intersection points
 		
-		sphere=new Sphere(new Point3D(0,0,-2), 4);//?????
-
+		sphere=new Sphere(Point3D.ZERO, 4);//?????
+		camera=new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,1,0)).setDistance(1).setViewPlaneSize(3, 3);
+		assertEquals("The count of intersections are not correct", 9, findIntersectionPoints(camera, sphere).size());	
 		
 		}
 		catch(Exception ex)
@@ -101,6 +107,48 @@ public class CameraRays_Geometries
 		}
 
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void IntegrationPlane() 
+	{	
+		try 
+		{
+			//TC01:9 intersection points
+			Plane plane =new Plane(new Point3D(2,0,0), new Vector(0,1,0));
+			Camera camera =new Camera(Point3D.ZERO, new Vector(0,1,0) , new Vector(0,-1,0));
+			//TC02:9 intersection points	
+			
+			//TC03:6 intersection points
+		} 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	public void IntegrationTriangle() 
+	{
+		//TC01:1 intersection points
+		try 
+		{
+			Triangle triangle=new Triangle(new Point3D(0,1,-2),new Point3D(1,-1,-2),new Point3D(-1,-1,-2));
+			Camera camera=new Camera(Point3D.ZERO, new Vector(0,0,-1), new Vector(0,1,0)).setDistance(1).setViewPlaneSize(3, 3);
+			assertEquals("The count of intersections are not correct", 1, findIntersectionPoints(camera, triangle).size());	
+			
+			//TC02:2 intersection points
+			triangle=new Triangle(new Point3D(0,20,-2),new Point3D(1,-1,-2),new Point3D(-1,-1,-2));
+			assertEquals("The count of intersections are not correct", 2, findIntersectionPoints(camera, triangle).size());	
+		} 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
 	}
 
 }
