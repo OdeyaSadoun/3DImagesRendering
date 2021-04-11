8ba225fe408ec01358b578ad2f0f488055f70f4b
אודיה סעדון <osadoun@g.jct.ac.il>
2021-04-11 08:52:20 +0300

    add javacode to interface Geometry

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,11 +1,14 @@
 package geometries;
-
-
 import primitives.Vector;
 import primitives.Point3D;
 
+/**
+ * interface Geometry for all the geometries that have normal
+ * 
+ * @author Tamar Gavrieli 322533977 & Odeya Sadoun 212380406
+ * */
 public interface Geometry extends Intersectable
 {
-	Vector getNormal(Point3D point) throws Exception;
+	Vector getNormal(Point3D point) throws Exception; // function get normal
 
 }
