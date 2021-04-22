382177b6b89dea261cf6ea1b8285b9e11d226816
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-04-22 21:26:58 +0300

    start rayTracerBasic

---
+++ src/renderer/RayTracerBasic.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package renderer;
+
+import scene.Scene;
+
+/**
+ * @author Home
+ *
+ */
+public class RayTracerBasic extends RayTracerBase 
+{
+
+	/**
+	 * @param myscene
+	 */
+	public RayTracerBasic(Scene myscene) 
+	{
+		super(myscene);
+		
+	}
+
+}
