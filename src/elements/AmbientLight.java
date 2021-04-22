c6a0fcfbfdf66950de5ef3962724497cffec9399
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-04-22 19:00:09 +0300

    some changes

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -18,18 +18,22 @@ public class AmbientLight
 	 */
 	
 	public Color Ia;
-	public double Ka;
-	public Color intensity;
+	private double Ka;
+	private Color intensity;
 	
 	public AmbientLight(Color Ia,double Ka ) 
 	{
 		this.intensity = Ia.scale(Ka);
 	}
 
-
-	public void setIntensity(Color intensity) 
+	/**
+	 * @return the intensity
+	 */
+	public Color getIntensity() 
 	{
-		this.intensity = intensity;
+		return intensity;
 	}
+
+
 
 }
