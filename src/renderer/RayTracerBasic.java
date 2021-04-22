751ee0a075d8e128715c52e89e1d61e93004c17b
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-04-22 22:23:37 +0300

    implement for RayTracerBasic

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -3,6 +3,11 @@
  */
 package renderer;
 
+import java.util.List;
+
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Ray;
 import scene.Scene;
 
 /**
@@ -21,6 +26,34 @@ public class RayTracerBasic extends RayTracerBase
 		super(myscene);
 		//return null;
 		
+	}
+	
+	public Color traceRay(Ray ray)
+	{
+		try 
+		{
+			List<Point3D> intersections = myscene.geometries.findIntersections(ray);
+			if(intersections==null)
+				return  myscene.background;
+			Point3D closestPoint = ray.findClosestPoint(intersections);
+			return calcColor(closestPoint);
+		} 
+		catch (Exception ex) 
+		{
+			// TODO Auto-generated catch block
+		
+		}
+		return null;
+	}
+	
+	/**
+	 * @param point Point3D value
+	 * @return Color
+	 * Function for calculating a point color
+	 */
+	private Color calcColor(Point3D point)
+	{
+		return myscene.ambientLight.getIntensity();
 	}
 
 }
