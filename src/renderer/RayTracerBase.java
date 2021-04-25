5cd4f1eb021844685f2ce31666d2048561e616e7
אודיה סעדון <osadoun@g.jct.ac.il>
2021-04-25 12:34:02 +0300

    some changes in javacode in RayTracerBase

--- src/renderer/RayTracerBase.java
+++ src/renderer/RayTracerBase.java
@@ -7,8 +7,9 @@ import primitives.Ray;
 import scene.Scene;
 
 /**
+ * class RayTracerBase of renderer package
+ * 
  * @author Tamar Gavrieli & Odeya Sadoun
- *
  */
 public abstract class RayTracerBase 
 {
@@ -16,17 +17,24 @@ public abstract class RayTracerBase
 	protected Scene myscene;
 	
 	/**
-	 * @param myscene Scene value	 */
+	 * constructor 
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @param myscene Scene value	
+	 */
 	public  RayTracerBase(Scene myscene)
 	{
 		this.myscene=myscene;
 	}
 	
 	/**
+	 * Statement of an abstract function that calculates the color for the nearest intersection point, 
+	 * if no intersection points are returned the color of the background	
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
 	 * @param ray Ray value
 	 * @throws Exception
 	 * @return Color
-	 * Statement of an abstract function that calculates the color for the nearest intersection point, 
-	 * if no intersection points are returned the color of the background	 */
+	 *  */
 	public abstract Color traceRay(Ray ray);
 }
