6534c6b48750bc0066b6d626d8f350f049070134
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-01 21:29:01 +0300

    change geometry from interface to abstract class in addition add filed color and geter and seter

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,5 +1,6 @@
 package geometries;
 import primitives.Vector;
+import primitives.Color;
 import primitives.Point3D;
 
 /**
@@ -7,7 +8,7 @@ import primitives.Point3D;
  * 
  * @author Tamar Gavrieli 322533977 & Odeya Sadoun 212380406
  * */
-public interface Geometry extends Intersectable
+public abstract class Geometry implements Intersectable
 {
 	/**
 	 * A function that return the normal of the geometry
@@ -16,6 +17,30 @@ public interface Geometry extends Intersectable
 	 * @param point
 	 * @return Vector value
 	 * */
-	Vector getNormal(Point3D point) throws Exception; // function get normal
+	protected  Color emmission = Color.BLACK;
+	public abstract Vector getNormal(Point3D point) throws Exception; // function get normal
+	
+	/**
+	 * getter function for the color filed in geometry class
+	 * 
+	 * @author Tamar Gavrieli 322533977 & Odeya Sadoun 212380406
+	 * @return emmission Color value
+	 * */
+	public Color getEmmission() 
+	{
+		return emmission;
+	}
+	
+	/**
+	 * setter function for the color filed and return this- geometry class
+	 * 
+	 * @author Tamar Gavrieli 322533977 & Odeya Sadoun 212380406
+	 * @return the geometry-this
+	 * */
+	public Geometry setEmmission(Color emmission) 
+	{
+		this.emmission = emmission;
+		return this;
+	}
 
 }
