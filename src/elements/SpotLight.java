e3d497e6c917cd0cdfe7c2509a331a16f9f79fe7
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-05-02 01:41:29 +0300

    create spotLight

---
+++ src/elements/SpotLight.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package elements;
+
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Vector;
+
+/**
+ * @author Home
+ *
+ */
+public class SpotLight extends PointLight 
+{
+
+	/**
+	 * @param intensity
+	 * @param position
+	 * @param KC
+	 * @param KL
+	 * @param KQ
+	 */
+	private Vector direction;
+	
+	public SpotLight(Color intensity, Point3D position, double KC, double KL, double KQ, Vector direction) 
+	{
+		super(intensity, position, KC, KL, KQ);
+		this.direction=direction;
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	public SpotLight() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
