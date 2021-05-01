ae9363aea446d6feb020b0c557af5a6ba4645fbd
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-02 01:43:12 +0300

    some changes in renderTest

--- src/unittests/RenderTests.java
+++ src/unittests/RenderTests.java
@@ -57,7 +57,7 @@ public class RenderTests
 		{
 			Scene scene = new Scene("Test scene").setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2));
 
-			scene.geometries.add(new Sphere(50, new Point3D(0, 0, -100)), //
+			scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50), //
 				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up left
 						.setEmission(new Color(java.awt.Color.GREEN)),
 				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
