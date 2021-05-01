7664f73ce4a8bf1df9d9198bd723eee918fddeb8
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-05-01 23:31:34 +0300

    add to test to getClosestGeoPoint

--- src/unittests/RayTest.java
+++ src/unittests/RayTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import geometries.Intersectable.GeoPoint;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
@@ -72,6 +73,39 @@ public class RayTest
 			
 		}
 		catch (Exception e) {}
+	}
+	
+	@Test
+	public void testGetClosestGeoPoint() 
+	{
+		try 
+		{
+			// =============== Boundary Values Tests ==================
+			Ray ray = new Ray(new Point3D(0,0,1), new Vector(1,0,0));
+			
+			GeoPoint p1 = new GeoPoint(new Point3D(1,0,0),null );
+			GeoPoint p2 = new GeoPoint(new Point3D(2,0,0), );
+			GeoPoint p3 = new GeoPoint(new Point3D(3,0,0), );
+			
+			//The first point is closest to the beginning of the foundation
+			List<GeoPoint >points = List.of(p1,p2,p3);
+			assertEquals("", p1, ray.getClosestGeoPoint(points));
+			
+			//The last point is closest to the beginning of the foundation
+			points = List.of(p2,p3,p1);
+			assertEquals("", p1, ray.getClosestGeoPoint(points));
+			
+			//An empty list
+			points = List.of(null);
+			assertEquals("", null, ray.getClosestGeoPoint(points));
+			
+			// ============ Equivalence Partitions Tests ==============
+			//A point in the middle of the list is closest to the beginning of the fund
+			points = List.of(p2,p1,p3);
+			assertEquals("", p1, ray.getClosestGeoPoint(points));
+			
+		}
+		catch (Exception ex) {}
 	}
 
 }
