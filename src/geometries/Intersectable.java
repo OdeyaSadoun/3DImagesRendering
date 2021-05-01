f8e00f64252b8e1adb8e386795ccdcfe09907bfa
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-01 21:49:33 +0300

    add abstract class in the interface

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -21,5 +21,44 @@ public interface Intersectable
 	 * @throws Exception
 	 * */
 	List<Point3D> findIntersections(Ray ray) throws Exception;
+	
+	/**
+	 * Static Internal Auxiliary Department (as a completely passive data structure - PDS)
+	 * 
+	 * @author Tamar Gavrieli 322533977 and Odeya Sadoun 212380406
+	 * @param geometry Geometry value
+	 * @param point Point3D value
+	 * */
+	public static class GeoPoint 
+	{
+	    public Geometry geometry;
+	    public Point3D point;
+	    
+	    
+	    /**
+	     * constructor for geo point
+	     * 
+	     * @author Tamar Gavrieli 322533977 and Odeya Sadoun 212380406
+	     * @param geometry Geometry
+	     * @param point Point3D
+	     * */
+	    public GeoPoint(Geometry geometry,Point3D point)
+	    {
+	    	this.geometry = geometry;
+	    	this.point = point;
+	    }
+	    
+		@Override
+		public boolean equals(Object obj) 
+		{
+			if (this == obj) return true;
+			if (obj == null) return false;
+			if (!(obj instanceof GeoPoint)) return false;
+			GeoPoint other = (GeoPoint)obj;
+			return this.geometry.equals(other.geometry) && this.point.equals(other.point);
+		}
+		
+	}
+
 
 }
