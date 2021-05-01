76c654ea10d73f8a9672d1ef9c6b5d1d256ad3ad
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-05-02 01:40:49 +0300

    creat lightSource

---
+++ src/elements/LightSource.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package elements;
+
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Vector;
+
+/**
+ * @author Home
+ *
+ */
+public interface LightSource 
+{
+	public Color getIntensity(Point3D p);
+	public Vector getL(Point3D p);
+
+
+}
