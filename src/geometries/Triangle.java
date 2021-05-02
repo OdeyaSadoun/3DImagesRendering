03401810d5791a445b4a2085a52e54e25cb0c249
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-02 10:15:33 +0300

    delete findintersection in triangle

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,7 +1,5 @@
 package geometries;
 import static primitives.Util.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import geometries.Intersectable.GeoPoint;
@@ -27,6 +25,8 @@ public class Triangle extends Polygon
 		super(p1,p2,p3);
 		
 	}
+	
+	
 
 	/************************************************************/
 	
@@ -36,43 +36,10 @@ public class Triangle extends Polygon
 		return "Triangle: "+super.toString();
 	}
 
-	@Override
-	public List<Point3D> findIntersections(Ray ray) throws Exception 
-	{
-		List<Point3D> rayPoints = plane.findIntersections(ray);
-		if (rayPoints == null)
-			return null;
-		//check if the point in out or on the triangle:
-		Vector v1 = vertices.get(0).subtract(ray.getP0());
-		Vector v2 = vertices.get(1).subtract(ray.getP0());
-		Vector v3 = vertices.get(2).subtract(ray.getP0());
-		
-		Vector n1 = v1.crossProduct(v2).normalize();
-		Vector n2 = v2.crossProduct(v3).normalize();
-		Vector n3 = v3.crossProduct(v1).normalize();
-
-		
-		//The point is inside if all 𝒗 ∙ 𝑵𝒊 have the same sign (+/-)
-		
-		if (alignZero(n1.dotProduct(ray.getDir())) > 0 && alignZero(n2.dotProduct(ray.getDir())) > 0 && alignZero(n3.dotProduct(ray.getDir())) > 0)
-		{
-			return rayPoints;
-		}
-		else if (alignZero(n1.dotProduct(ray.getDir())) < 0 && alignZero(n2.dotProduct(ray.getDir())) < 0 && alignZero(n3.dotProduct(ray.getDir())) < 0)
-		{
-			return rayPoints;
-		}
-		if (isZero(n1.dotProduct(ray.getDir())) || isZero(n2.dotProduct(ray.getDir())) || isZero(n3.dotProduct(ray.getDir())))
-			return null; //there is no instruction point
-		return null;
-	}
-	
 
 	@Override
 	public List<GeoPoint> findGeoIntersections(Ray ray) throws Exception 
 	{
-
-		
 		List<GeoPoint> rayPoints = plane.findGeoIntersections(ray);
 		if (rayPoints == null)
 			return null;
