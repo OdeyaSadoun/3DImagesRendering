39d5c6ae719e7d86969b0837adb585e814de4007
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-06 22:38:50 +0300

    change in the vectors from Dan in rendertest

--- src/unittests/RenderTests.java
+++ src/unittests/RenderTests.java
@@ -15,7 +15,7 @@ import scene.Scene;
 public class RenderTests 
 {
 	
-	private Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, -1, 0)).setDistance(100).setViewPlaneSize(500, 500);
+	private Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)).setDistance(100).setViewPlaneSize(500, 500);
 	
 
 	/**
