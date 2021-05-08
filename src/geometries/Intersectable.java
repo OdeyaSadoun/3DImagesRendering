1429eb12f45378ed891707f075b13dcd61e143c1
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-09 01:30:16 +0300

    change to throws IllegalArgumentException in intersectable

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -29,7 +29,7 @@ public interface Intersectable
 	}
 
 	
-	List<GeoPoint> findGeoIntersections (Ray ray) throws Exception;
+	List<GeoPoint> findGeoIntersections (Ray ray) throws IllegalArgumentException;
 	
 	
 	/**
