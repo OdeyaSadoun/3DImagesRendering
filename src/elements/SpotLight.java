9d169a668ad6f320036feb61dc00a8f0dd369f27
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-09 00:15:17 +0300

    add javacode and some changes to spotlight

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -1,34 +1,63 @@
-/**
- * 
- */
+
 package elements;
+
+import static primitives.Util.alignZero;
 
 import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
 
 /**
- * @author Home
- *
+ * class SpotLight that extends from class PointLight
+ * 
+ * @author Tamar Gavrieli & Odeya Sadoun
  */
 public class SpotLight extends PointLight 
 {
+	private Vector direction;
 
 	/**
-	 * @param intensity
-	 * @param position
-	 * @param KC
-	 * @param KL
-	 * @param KQ
+	 * constructor for spotlight that receives all the params
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @param direction Vector value 
+	 * @param intensity Color value
+	 * @param position Point3D value
+	 * @param KC double value
+	 * @param KL double value
+	 * @param KQ double value
 	 */
-	private Vector direction;
-	
 	public SpotLight(Color intensity, Point3D position, double KC, double KL, double KQ, Vector direction) 
 	{
 		super(intensity, position, KC, KL, KQ);
-		this.direction=direction;
+		this.direction=direction.normalize();
 	}
 
+	/**
+	 * constructor for spotlight that receives 3 params
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @param direction Vector value 
+	 * @param intensity Color value
+	 * @param position Point3D value
+	 */
+	public SpotLight(Color intensity, Point3D position, Vector direction) 
+	{
+		super(intensity, position);
+		this.direction=direction.normalize();
+	}
+	
+	
+	/*******************************************************************************/
+	@Override
+	public Color getIntensity(Point3D p) throws IllegalArgumentException
+	{
+		if(this.getL(p) == null)
+			return Color.BLACK;
+		if (alignZero(direction.dotProduct(this.getL(p))) <= 0)
+			return Color.BLACK;
+		return super.getIntensity(p).scale(alignZero(direction.dotProduct(this.getL(p))));
+	}
 
 
 }
