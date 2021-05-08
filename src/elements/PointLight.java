0af2979ec63b408874ffc057c25e60cc1938024b
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-08 23:48:27 +0300

    some changes and add javacode in pointlight

--- src/elements/PointLight.java
+++ src/elements/PointLight.java
@@ -8,8 +8,10 @@ import primitives.Point3D;
 import primitives.Vector;
 
 /**
- * @author Home
- *
+ * class PointLight that extends from class light
+ * and implement the interface LightSource
+ * 
+ * @author Tamar Gavrieli & Odeya Sadoun
  */
 public class PointLight extends Light implements LightSource
 {
@@ -18,8 +20,18 @@ public class PointLight extends Light implements LightSource
 	private double KC = 1;
 	private double KL = 0;
 	private double KQ = 0;
+	
+	
+	
 	/**
-	 * @param intensity
+	 * constructor of PointLight that receives all the params
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @param intensity Color value
+	 * @param position Point3D value
+	 * @param KC double value
+	 * @param KL double value
+	 * @param KQ double value
 	 */
 	public PointLight(Color intensity, Point3D position, double KC, double KL, double KQ) 
 	{
@@ -28,19 +40,35 @@ public class PointLight extends Light implements LightSource
 		this.KC=KC;
 		this.KL=KL;
 		this.KQ=KQ;
-		
+	}
+	
+	/**
+	 * constructor of PointLight that receives two params
+	 * kc,kl,kq are with default value
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @param intensity Color value
+	 * @param position Point3D value
+	 */
+	public PointLight(Color intensity, Point3D position)
+	{
+		super(intensity);
+		this.position = position;
 	}
 
+	/**************************************************************************************/
 	@Override
-	public Color getIntensity(Point3D p) throws Exception
+	public Color getIntensity(Point3D p) throws IllegalArgumentException
 	{
-		return super.getIntensity();
-		
+		return getIntensity().reduce((KC + KL * p.distance(position)+ KQ * p.distanceSquared(position)));	
 	}
+	
 	@Override
-	public Vector getL(Point3D p) throws Exception 
+	public Vector getL(Point3D p) throws IllegalArgumentException 
 	{	
-		return position.subtract(p);		
+		if (p.equals(position))
+			return null; //In order not to reach a state of exception due to the zero vector
+		return position.subtract(p).normalize();		
 	}
 
 }
