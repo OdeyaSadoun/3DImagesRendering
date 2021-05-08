32fb172f729cc0f9f14ebf1fba39a854cc8ef381
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-08 23:18:59 +0300

    add javacode to light

--- src/elements/Light.java
+++ src/elements/Light.java
@@ -1,36 +1,38 @@
-/**
- * 
- */
 package elements;
-
 import primitives.Color;
 
+
 /**
- * @author Home
- *
+ * class for light - abstract class
+ * 
+ * @author Tamar Gavrieli & Odeya Sadoun
  */
  abstract class Light 
  {
 
-	/**
-	 * 
-	 */
-	 
 	protected Color intensity;
-	/**
-	 * @return the intensity
-	 * 
-	 */
 	
+	
+	/**
+	 * constructor for light
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @return the intensity
+	 */
 	protected Light(Color intensity)
 	{
 		this.intensity=intensity;
 	}
  
+	/**
+	 * getter to intensity
+	 * 
+	 * @author Tamar Gavrieli & Odeya Sadoun
+	 * @return intensity Color 
+	 * */
 	public Color getIntensity() 
 	{
 		return intensity;
 	}
-
 
 }
