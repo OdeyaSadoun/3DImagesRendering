5f587734455ecd86ba91507492663320fd9dd8e7
אודיה סעדון <osadoun@g.jct.ac.il>
2021-05-09 11:24:49 +0300

    replacce the tests withw the new that DAN gave in rendertest

--- src/unittests/RenderTests.java
+++ src/unittests/RenderTests.java
@@ -1,63 +1,68 @@
+
 package unittests;
-import static org.junit.Assert.*;
+
 import org.junit.Test;
+
 import elements.*;
 import geometries.*;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
+
 /**
- * test class for render class
- *  
+ * Test rendering a basic image
+ * 
  * @author Tamar Gavrieli & Odeya Sadoun
- *
  */
-public class RenderTests 
-{
-	
-	private Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)).setDistance(100).setViewPlaneSize(500, 500);
-	
+public class RenderTests {
+	private Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+			.setDistance(100) //
+			.setViewPlaneSize(500, 500);
 
 	/**
-	 * Produce a scene with basic 3D model and render it into a jpeg image with a
+	 * Produce a scene with basic 3D model and render it into a png image with a
 	 * grid
 	 */
 	@Test
 	public void basicRenderTwoColorTest() 
 	{
+		Scene scene = new Scene("Test scene")//
+				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1)) //
+				.setBackground(new Color(75, 127, 90));
 
-		try
-		{
-			Scene scene = new Scene("Test scene").setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1)).setBackground(new Color(75, 127, 90));
+		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50),
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up
+																													// left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up
+																													// right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)), // down
+																														// left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down
+																													// right
 
-			scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50),
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up left
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)), // down left
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down right
+		ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
+		Render render = new Render() //
+				.setImageWriter(imageWriter) //
+				.setCamera(camera) //
+				.setRayTracer(new RayTracerBasic(scene));
 
-			ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
-			Render render = new Render().setImageWriter(imageWriter).setScene(scene).setCamera(camera).setRayTracer(new RayTracerBasic(scene));
-			render.renderImage();
-			render.printGrid(100, new Color(java.awt.Color.YELLOW));
-			render.writeToImage();
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-			fail("not need exception!");
-		}
-
+		render.renderImage();
+		render.printGrid(100, new Color(java.awt.Color.YELLOW));
+		render.writeToImage();
 	}
-	
-	@Test
-	public void basicRenderMultiColorTest() 
-	{
-		try
-		{
-			Scene scene = new Scene("Test scene").setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2));
 
-			scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50), //
+	// For stage 6 - please disregard in stage 5
+	/**
+	 * Produce a scene with basic 3D model - including individual lights of the bodies 
+	 * and render it into a png image with a grid
+	 */
+	@Test
+	public void basicRenderMultiColorTest() {
+		Scene scene = new Scene("Test scene")//
+				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2)); //
+
+		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50) //
+				.setEmission(new Color(java.awt.Color.CYAN)), //
 				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up left
 						.setEmission(new Color(java.awt.Color.GREEN)),
 				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
@@ -66,21 +71,14 @@ public class RenderTests
 				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100)) // down right
 						.setEmission(new Color(java.awt.Color.BLUE)));
 
-			ImageWriter imageWriter = new ImageWriter("color render test", 1000, 1000);
-			Render render = new Render() //
+		ImageWriter imageWriter = new ImageWriter("color render test", 1000, 1000);
+		Render render = new Render() //
 				.setImageWriter(imageWriter) //
-				.setScene(scene) //
 				.setCamera(camera) //
 				.setRayTracer(new RayTracerBasic(scene));
 
-			render.renderImage();
-			render.printGrid(100, new Color(java.awt.Color.WHITE));
-			render.writeToImage();
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-			fail("not need exception!");
-		}
+		render.renderImage();
+		render.printGrid(100, new Color(java.awt.Color.WHITE));
+		render.writeToImage();
 	}
 }
