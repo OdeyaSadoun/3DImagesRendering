82107624664ce7befa5426992aad1ec6f5d66c73
Tamar Gavrieli <tgavriel@g.jct.ac.il>
2021-05-18 00:26:05 +0300

    Add fields to Materiel class

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -17,7 +17,28 @@ public class Material
 	public int nShininess=0; //עד כמה החומר בוהק לעצמו
 	public double KD=0; //כמה החומר מחזיר אור
 	public double KS=0; //כמה החומר מחזיר בוהק
+	public double KT=0.0; //Promotes transparency מקדם שקיפות
+	public double KR=0.0; //Coefficient of reflection מקדם השתקפות
 	
+	
+	/**
+	 * @param kT the kT to set
+	 */
+	public Material setKT(double kT) 
+	{
+		KT = kT;
+		return this;
+	}
+
+	/**
+	 * @param kR the kR to set
+	 */
+	public Material setKR(double kR) 
+	{
+		KR = kR;
+		return this;
+	}
+
 	/**
 	 * @param nShininess the nShininess to set
 	 */
