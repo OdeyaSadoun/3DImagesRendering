931159a5c44ac0fa5411403660ebd9c249ae7f49
אודיה סעדון <osadoun@g.jct.ac.il>
2021-06-13 13:53:48 +0300

    change in syntax in color class

--- src/primitives/Color.java
+++ src/primitives/Color.java
@@ -184,7 +184,7 @@ public class Color
 	public Color reduce(double k) 
 	{
 		if (k < 1)
-			throw new IllegalArgumentException("Can't scale a color by a by a number lower than 1");
+			throw new IllegalArgumentException("Can't scale a color by a number lower than 1");
 		return new Color(r / k, g / k, b / k);
 	}
 
